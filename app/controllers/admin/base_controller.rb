f723aefffedd2d23d2b5190ee6490378a98ede38
soliveira <mucabatera@gmail.com>
2024-01-29 08:53:12 -0300

    REFS #-ecomm por Samuel Oliveira: Restringindo o acesso à home do Admin

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,14 @@
 module Admin
     class BaseController < ApplicationController
         layout 'admin'
+        before_action :authenticate_user!
+        before_action :only_user!
+
+
+        private
+
+        def only_user!
+            redirect_to root_path unless current_user.admin?
+        end
     end
 end
