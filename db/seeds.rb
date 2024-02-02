b449d3d6ee4fd2f96a1e394047ada70cd0a05d0b
soliveira <mucabatera@gmail.com>
2024-02-02 15:03:12 -0300

    REFS #e-comm por Samuel Oliveira: Docker-compose pronto

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(name: 'Samuel', email: 'mucabatera-admin@gmail.com', phone: '21912345678',password: '123456', password_confirmation: '123456', role: 'admin')
+cat1 = Category.create!(name: 'Plastificações', position: 1, )
+cat1.products.create!(name: 'Calendário A4', description: "Calendário no tamanho A4, personalizado. Time: Flamengo",
+price: 10.90, publish: true)
+cat1.products.create!(name: 'Calendário A4', description: "Calendário no tamanho A4, personalizado. Tema: Natal",
+price: 10.90, publish: true)
+cat1.products.create!(name: 'Calendário A4', description: "Calendário no tamanho A4, personalizado. Tema: Java",
+price: 10.90, publish: true)
