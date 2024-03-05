455a7b5775893f97dffa96d2d67e4c7fb6c630bd
soliveira <mucabatera@gmail.com>
2024-03-05 14:41:54 -0300

    REFES #storage por Samuel Oliveira: Acertando o tamanho dos produtos na index 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
     attachable.variant :cart_avatar, resize_to_limit: [45, 45]
     attachable.variant :small_avatar, resize_to_limit: [100, 100]
     attachable.variant :small_thumb, resize_to_limit: [150, 150]
-    attachable.variant :mid1_avatar, resize_to_limit: [200, 200]
+    attachable.variant :mid1_avatar, resize_to_limit: [180, 180]
     attachable.variant :mid1_thumb, resize_to_limit: [216, 216]
     attachable.variant :mid2_thumb, resize_to_limit: [300, 300]
     attachable.variant :big1_thumb, resize_to_limit: [380, 380]
