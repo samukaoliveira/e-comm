a23068a66df880e4ff86335e5a7fb0a660fb34d0
soliveira <mucabatera@gmail.com>
2024-03-06 08:33:21 -0300

    REFES #admin por Samuel Oliveira: Acertando o avatar de user com os resizes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image do |attachable|
-    attachable.variant :small_avatar, resize_to_limit: [100, 100]
-    attachable.variant :mid1_avatar, resize_to_limit: [200, 200]
-  end
+  attachable.variant :cart_avatar, resize_to_limit: [45, 45]
+  attachable.variant :small_avatar, resize_to_limit: [100, 100]
+  attachable.variant :small_thumb, resize_to_limit: [150, 150]
+  attachable.variant :mid1_avatar, resize_to_limit: [176, 176]
+  attachable.variant :mid1_thumb, resize_to_limit: [216, 216]
+  attachable.variant :mid2_thumb, resize_to_limit: [300, 300]
+  attachable.variant :big1_thumb, resize_to_limit: [380, 380]
+  attachable.variant :big2_thumb, resize_to_limit: [450, 450]
+  attachable.variant :big3_thumb, resize_to_limit: [600, 600]
+end
 
   validates :name, :phone, presence: true
 
